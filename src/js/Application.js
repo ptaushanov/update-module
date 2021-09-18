149e7b08925d374271aca9f315a699d4b60fbe3a
ptaushanov <pepi15032000@gmail.com>
2021-09-18 19:04:33 +0300

    feat: added animation to article

--- src/js/Application.js
+++ src/js/Application.js
@@ -12,5 +12,17 @@ export default class Application extends EventEmitter {
     this.init();
     this.emit(Application.events.READY);
   }
-  init() {}
+  init() {
+    document
+    .querySelector('article')
+    .addEventListener('click', () => {
+      anime({
+        targets: 'article',
+        translateX: 250,
+        direction: 'alternate',
+        loop: true,
+        easing: 'spring(1, 80, 10, 0)'
+      });
+    })
+  }
 }
